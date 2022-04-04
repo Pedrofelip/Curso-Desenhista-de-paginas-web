af7cf08914af643c0dd4fd2c8339250dc4c917fc
pedrofelip <pedrofelipbarros@gmail.com>
2022-04-04 19:42:38 -0300

    projeto final finalizado

--- projeto final/script.js
+++ projeto final/script.js
@@ -49,4 +49,18 @@ function sliderAuto(slider, miliseconds) {
  slide();
 }
 
-// sliderAuto(glider, 5000)
+sliderAuto(glider, 5000)
+
+// teste side Bar
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
